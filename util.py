78489b97305051f56299c446df9f5cd488a7ebff
zhangxulong <zhangxulong@ieee.org>
2023-07-06 17:46:44 +0800

    util to remove the dublicated publication

---
+++ util.py
@@ -0,0 +1,39 @@
+import os
+import shutil
+def filter_dubplicate():
+    jzwang_dir='content/publication/jzwang'
+    exist_title=''
+    for item in range(2007,2023):
+        other_dir=os.path.join('content/publication/',str(item))
+        if os.path.isdir(other_dir):
+            for root,dirs,names in os.walk(other_dir):
+                for file_name in names:
+                    file_path=os.path.join(root,file_name)
+                    if 'index.md' in file_path:
+                        with open(file_path) as f_p:
+                            all_lines=f_p.readlines()
+                            for line in all_lines:
+                                if 'title' in line:
+                                    exist_title+=line.lower()
+    count=0
+    for root,dirs,names in os.walk(jzwang_dir):
+        for file_name in names:
+                    file_path=os.path.join(root,file_name)
+                    if 'index.md' in file_path:
+                        with open(file_path) as f_p:
+                            all_lines=f_p.readlines()
+                            for line in all_lines:
+                                if 'title' in line:
+                                     the_title=line.split('"')[1].lower()
+                                     if the_title in exist_title:
+                                            count+=1
+                                            print("%02d   %s"%(count,the_title))
+                                            print(root)
+                                            shutil.rmtree(root)
+
+
+    return 0
+
+if __name__ == '__main__':
+    filter_dubplicate()
+    
